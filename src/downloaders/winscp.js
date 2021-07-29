034e3829f51ec1506010d33c665fc9946e16407f
TLZ <roesch.eric@protonmail.com>
2021-07-29 11:19:29 +0200

    Automatically download latest version of WinSCP

--- src/downloaders/winscp.js
+++ src/downloaders/winscp.js
@@ -4,45 +4,60 @@ module.exports = (cli) => {
   const path = require('path')
   const fs = require('fs')
 
-  const url =
-    'https://netcologne.dl.sourceforge.net/project/winscp/WinSCP/5.19.2/WinSCP-5.19.2-Portable.zip'
-
-  https.get(url, (res) => {
+  https.get('https://winscp.net/eng/downloads.php', (res) => {
     let data = []
-
-    res.on('data', (chunk) => data.push(chunk))
+    res.on('data', (chunk) => (data += chunk))
     res.on('end', () => {
-      data = Buffer.concat(data)
+      const version = data.match(/WinSCP-(.*?)-Portable.zip/)[1]
 
-      JSZip.loadAsync(data).then((zip) => {
-        if (!fs.existsSync(cli.config.dataDir)) {
-          fs.mkdirSync(cli.config.dataDir, { recursive: true })
-        }
+      const url =
+        'https://netcologne.dl.sourceforge.net/project/winscp/WinSCP/' +
+        version +
+        '/WinSCP-' +
+        version +
+        '-Portable.zip'
 
-        zip
-          .file('WinSCP.com')
-          .async('nodebuffer')
-          .then((res) => {
-            fs.writeFileSync(
-              path.resolve(cli.config.dataDir, 'WinSCP.com'),
-              res
-            )
-            fs.chmodSync(path.resolve(cli.config.dataDir, 'WinSCP.com'), 0o755)
+      https.get(url, (res) => {
+        let data = []
+
+        res.on('data', (chunk) => data.push(chunk))
+        res.on('end', () => {
+          data = Buffer.concat(data)
+
+          JSZip.loadAsync(data).then((zip) => {
+            if (!fs.existsSync(cli.config.dataDir)) {
+              fs.mkdirSync(cli.config.dataDir, { recursive: true })
+            }
 
             zip
-              .file('WinSCP.exe')
+              .file('WinSCP.com')
               .async('nodebuffer')
               .then((res) => {
                 fs.writeFileSync(
-                  path.resolve(cli.config.dataDir, 'WinSCP.exe'),
+                  path.resolve(cli.config.dataDir, 'WinSCP.com'),
                   res
                 )
                 fs.chmodSync(
-                  path.resolve(cli.config.dataDir, 'WinSCP.exe'),
+                  path.resolve(cli.config.dataDir, 'WinSCP.com'),
                   0o755
                 )
+
+                zip
+                  .file('WinSCP.exe')
+                  .async('nodebuffer')
+                  .then((res) => {
+                    fs.writeFileSync(
+                      path.resolve(cli.config.dataDir, 'WinSCP.exe'),
+                      res
+                    )
+                    fs.chmodSync(
+                      path.resolve(cli.config.dataDir, 'WinSCP.exe'),
+                      0o755
+                    )
+                  })
               })
           })
+        })
       })
     })
   })
